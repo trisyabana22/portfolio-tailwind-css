f9415f22182355338a7978ea12a5c9d144a94e44
trisyabana22 <cobainaja1111@gmail.com>
2022-04-18 13:37:27 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center:true,
+      // padding:'16px'
+      padding: {
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      }
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary:'#64748b',
+        dark:'#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
