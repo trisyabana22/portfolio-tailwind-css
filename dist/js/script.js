de46d61d64859a85b4c467396edc2f3f254cbfb9
trisyabana22 <cobainaja1111@gmail.com>
2022-04-19 10:39:45 +0700

    fix bug and add dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top')
 
     if(window.pageYOffset > fixedNav){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex')
+        toTop.classList.add('hidden')
     }
 }
 
@@ -18,5 +23,42 @@ const navMenu = document.querySelector('#nav-menu')
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+    console.log('clicked');
     
 })
+
+
+
+
+// klik diluar Hamburger
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+        console.log('kliked');
+    }
+});
+
+
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if(darkToggle.checked) {
+        html.classList.add('dark')
+        localStorage.theme = 'dark'
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light'
+        
+    }
+})
+
+// Pindahkan Toggle Sesuai Mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true
+} else {
+    darkToggle.checked = false
+  }
